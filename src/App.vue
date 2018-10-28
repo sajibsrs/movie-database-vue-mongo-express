951fbdbd567bf4ca9e7f3829afd3ada0f4e40bc2
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-29 05:16:45 +0600

    home page component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home title="Welcome to MDB" desc="A movie database"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
