4f0a6b062cb027e2159e8b319a9983cfefb956a4
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-29 16:39:56 +0600

    main home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ title }}</h1>
-    <p>{{ desc }}</p>
+    <h2 class="page-title">This is the home page</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    desc: String
-  }
+  name: 'Home'
 }
 </script>
-
-<!-- Add "scoped" attribute to
-limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
