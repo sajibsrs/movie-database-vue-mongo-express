ecd4790f64d30f9cec118edef386bd3b65d71d4f
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-29 16:38:17 +0600

    router added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router/main'
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
