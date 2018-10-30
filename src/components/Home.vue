921750c7b39b90e2c774d35d23d76e2beedff29c
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-30 15:27:57 +0600

    added some dummy data for now

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
     <h2 class="page-title">This is the home page</h2>
+    <div class="movies">
+      <div class="movie">
+        <h3 class="title">Movie title - year</h3>
+        <p class="director">Director name</p>
+        <p class="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam consectetur minima nesciunt repellat inventore odit accusantium vero, voluptatem veniam suscipit culpa ab ad corrupti molestias esse sit hic, perferendis fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione iusto soluta pariatur eaque qui alias ex, magni nulla molestias perferendis explicabo harum, minus aspernatur praesentium voluptate dolore inventore accusantium dicta?
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
