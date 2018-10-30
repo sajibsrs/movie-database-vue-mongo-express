4351cf75e369d6b3da1cd5c7bf88a81367357f15
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-31 04:01:19 +0600

    add movies data process and insert into database

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,10 +1,10 @@
 <template>
-  <form class="add-movie-form form-wrapper" @submit="validation" action="/movies" method="post">
+  <form class="add-movie-form form-wrapper" @submit.prevent="processForm" method="post">
     <p v-if="errors.length">
       <strong>
         Please correct the folllowing error(s):
       </strong>
-      <ul>
+      <ul class="validation-info">
         <li v-for="error in errors" :key="error.id">
           {{ error }}
         </li>
@@ -14,13 +14,15 @@
     <input v-model="name" type="text" name="name" id="name">
     <label for="year">Release Year:</label>
     <input v-model="year" type="number" name="year" id="year">
-    <label for="description">Description</label>
-    <textarea v-model="description" name="description" id="description" cols="30" rows="10"></textarea>
+    <label for="">Description</label>
+    <textarea v-model="description" name="description" id="description" placeholder="Enter description here"></textarea>
     <input type="submit" value="Submit">
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'AddMovie',
   data: () => ({ 
@@ -30,27 +32,44 @@ export default {
       description: null
   }),
   methods: {
-    validation: function(e) {
-      if (this.name && this.year && this.description) {
-        return true;
-      }
+    processForm: function(e) {
 
       this.errors = [];
 
       if (!this.name) {
-        this.errors.push('Name is required.');
+        this.errors.push('Name is required');
       }
 
       if (!this.year) {
-        this.errors.push('Year is required.');
+        this.errors.push('Year is required');
       }
 
       if (!this.description) {
-        this.errors.push('Description is required.');
+        this.errors.push('Description is required');
       }
 
-      e.preventDefault();
+      // return false if there is any validation error(s)
+      if (this.errors.length > 0){
+        // console.error('Check for form validation error(s)');
+        return false;
+      }
       
+      return (
+        axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            year: this.year,
+            description: this.description
+          },
+          url: 'http://localhost:8082/movies',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }).then(() => {
+          this.$router.push({ name: 'Home' });
+        })
+      );
     }
   }
 }
