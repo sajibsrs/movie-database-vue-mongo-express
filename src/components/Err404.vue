fc4aedd218baa22d8b40b0288af5038c840f424a
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-31 04:01:44 +0600

    404 page component added

---
+++ src/components/Err404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="err404">
+    <h2>404</h2>
+    <p>Sorry the content you are looking for doesnt exists.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Err404'
+}
+</script>
+
