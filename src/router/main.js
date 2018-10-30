5544a628a01ba8e69944f86f9f4cd23a1ffe8e91
Sajidur Rahman <sajibsrs@gmail.com>
2018-10-30 15:28:21 +0600

    add new movie route

--- src/router/main.js
+++ src/router/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home.vue'
 import Contact from '@/components/Contact.vue'
+import AddMovie from '@/components/AddMovie.vue'
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/contact',
       name: 'Contact',
       component: Contact
+    },
+    {
+      path: '/movies/add',
+      name: 'AddMovie',
+      component: AddMovie
     }
   ]
 });
