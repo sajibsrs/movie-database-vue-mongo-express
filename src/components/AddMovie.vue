14692b7a971dba857a1b2615cf4eff06dc213076
Sajidur Rahman <sajibsrs@gmail.com>
2018-11-01 05:16:43 +0600

    markup improvement

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,22 +1,34 @@
 <template>
   <form class="add-movie-form form-wrapper" @submit.prevent="processForm" method="post">
-    <p v-if="errors.length">
-      <strong>
-        Please correct the folllowing error(s):
-      </strong>
-      <ul class="validation-info">
-        <li v-for="error in errors" :key="error.id">
-          {{ error }}
-        </li>
-      </ul>
-    </p>
-    <label for="name">Movie Name:</label>
-    <input v-model="name" type="text" name="name" id="name">
-    <label for="year">Release Year:</label>
-    <input v-model="year" type="number" name="year" id="year">
-    <label for="">Description</label>
-    <textarea v-model="description" name="description" id="description" placeholder="Enter description here"></textarea>
-    <input type="submit" value="Submit">
+
+    <div class="input-wrapper">
+      <p v-if="errors.length">
+        <strong>
+          Please correct the folllowing error(s):
+        </strong>
+        <ul class="validation-info">
+          <li v-for="error in errors" :key="error.id">
+            {{ error }}
+          </li>
+        </ul>
+      </p>
+    </div>
+    
+    <div class="input-wrapper">
+      <label for="name">Movie Name:</label>
+      <input v-model="name" type="text" name="name" id="name">
+    </div>
+    <div class="input-wrapper">
+      <label for="year">Release Year:</label>
+      <input v-model="year" type="number" name="year" id="year">
+    </div>
+    <div class="input-wrapper">
+      <label for="">Description</label>
+      <textarea v-model="description" name="description" id="description"></textarea>
+    </div>
+    <div class="input-wrapper">
+      <input type="submit" value="Submit">
+    </div>
   </form>
 </template>
 
@@ -29,7 +41,8 @@ export default {
       errors: [],
       name: null,
       year: null,
-      description: null
+      description: null,
+      slug: null
   }),
   methods: {
     processForm: function(e) {
@@ -60,7 +73,8 @@ export default {
           data: {
             name: this.name,
             year: this.year,
-            description: this.description
+            description: this.description,
+            slug: (this.name).toLowerCase().split(' ').join('-')
           },
           url: 'http://localhost:8082/movies',
           headers: {
